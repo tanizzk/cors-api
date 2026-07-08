595522570fe564969500df5f462da0dd01732aee
tanizzk <23f3004469@ds.study.iitm.ac.in>
2026-07-08 22:24:07 +0530

    add config file

---
+++ config.py
@@ -0,0 +1,29 @@
+EMAIL = "23f3004469@ds.study.iitm.ac.in"
+
+# Q2 JWT
+PUBLIC_KEY_PEM = """
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----
+"""
+
+ISSUER = "https://idp.exam.local"
+
+AUDIENCE = "tds-076621u5.apps.exam.local"
+
+
+# Q3 defaults
+Q3_PORT = 8786
+Q3_WORKERS = 10
+Q3_DEBUG = True
+Q3_LOG_LEVEL = "warning"
+
+
+# Q9
+Q9_TOTAL_ORDERS = 50
